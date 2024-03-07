6963549f745f143ba52fee6bd14689591575c430
inesachour <ines.achour@insat.ucar.tn>
2024-03-07 10:36:08 +0100

    first

---
+++ README.md
@@ -0,0 +1,29 @@
+# Awesome Calendar Flutter Package
+
+## Overview
+The "Awesome Calendar" Flutter package offer a calendar widget designed to enhance your Flutter applications with a stylish and functional calendar display. </br>
+The package provides the AwesomeCalendar class, which can be easily integrated into your Flutter projects.
+
+## Usage
+### Import the package
+
+```dart
+import 'package:awesome_calendar/awesome_calendar.dart';
+```
+
+### Instantiate the AwesomeCalendar widget
+
+```dart
+AwesomeCalendar(
+  isDarkMode: true,
+  borderRadius: 16.0,
+  elevation: 4.0,
+),
+```
+
+## Parameters
+
+- isDarkMode (optional): A boolean parameter indicating whether the calendar should use a dark mode theme. If true, the calendar will be styled with a dark color scheme; otherwise, it will use a light color scheme. Default is false.
+- borderRadius (optional): A double parameter specifying the border radius of the calendar widget. This controls the rounded corners of the calendar. Default is 8.0.
+- elevation (optional): A double parameter representing the elevation of the calendar. This controls the shadow effect beneath the calendar. Default is 2.0.
+
---
+++ lib/awesome_calendar.dart
@@ -0,0 +1,153 @@
+library awesome_calendar;
+
+import 'package:flutter/material.dart';
+import 'package:awesome_calendar/theme.dart';
+import 'package:intl/intl.dart';
+
+class AwesomeCalendar extends StatefulWidget {
+  bool isDarkMode;
+  double elevation;
+  double borderRadius;
+
+  AwesomeCalendar({
+    super.key,
+    this.isDarkMode = false,
+    this.elevation = 5,
+    this.borderRadius = 20,
+  });
+
+  @override
+  State<AwesomeCalendar> createState() => _AwesomeCalendarState();
+}
+
+class _AwesomeCalendarState extends State<AwesomeCalendar> {
+
+  late DateTime today, firstDayInMonth, selectedDate;
+  late int daysInMonth;
+  List<String> days = ["MON", "TUE", "WED", "THU", "FRI", "SUN", "SAT"];
+  late dynamic theme;
+
+  String _getMonthAndYear(DateTime date) {
+    return DateFormat('MMMM yyyy').format(date);
+  }
+
+  _getNextMonth(){
+    if(firstDayInMonth.month != 12){
+      firstDayInMonth = DateTime(firstDayInMonth.year, firstDayInMonth.month + 1, 1);
+    }
+    else{
+      firstDayInMonth = DateTime(firstDayInMonth.year + 1, 1, 1);
+    }
+    daysInMonth = DateTimeRange(start: firstDayInMonth, end: DateTime(firstDayInMonth.year, firstDayInMonth.month + 1)).duration.inDays;
+    setState(() {});
+  }
+
+  _getPreviousMonth(){
+    if(firstDayInMonth.month != 1){
+      firstDayInMonth = DateTime(firstDayInMonth.year, firstDayInMonth.month - 1, 1);
+    }
+    else{
+      firstDayInMonth = DateTime(firstDayInMonth.year - 1, 12, 1);
+    }
+    daysInMonth = DateTimeRange(start: firstDayInMonth, end: DateTime(firstDayInMonth.year, firstDayInMonth.month + 1)).duration.inDays;
+    setState(() {});
+  }
+
+  _isDateSelected(int day, int month, int year){
+    return day == selectedDate.day && month == selectedDate.month && year == selectedDate.year;
+  }
+
+  @override
+  void initState() {
+    today = DateTime.now();
+    selectedDate = today;
+    firstDayInMonth = DateTime(today.year, today.month, 1);
+    daysInMonth = DateTimeRange(start: firstDayInMonth, end: DateTime(firstDayInMonth.year, firstDayInMonth.month + 1)).duration.inDays;
+    theme = widget.isDarkMode ? DarkTheme() : LightTheme();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: widget.elevation,
+      borderRadius: BorderRadius.circular(widget.borderRadius),
+      color: theme.backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        child: Column(
+          children: [
+
+            Row(
+              children: [
+                IconButton(
+                  onPressed: _getPreviousMonth,
+                  icon: Icon(Icons.arrow_back_ios, color: theme.primaryColor,),
+                ),
+
+                Expanded(
+                  child: Text(
+                    _getMonthAndYear(firstDayInMonth),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: theme.primaryColor, fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                IconButton(
+                  onPressed: _getNextMonth,
+                  icon: Icon(Icons.arrow_forward_ios, color: theme.primaryColor,),
+                ),
+              ],
+            ),
+
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 7,
+              childAspectRatio: 1.5,
+              padding: const EdgeInsets.all(0),
+              children: List<Widget>.generate(7, (i) {
+                return Center(
+                    child: Text(
+                      days[i],
+                      style: TextStyle(color: theme.secondaryColor),
+                    ),
+                );
+              }),
+            ),
+
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 7,
+              childAspectRatio: 1.5,
+              padding: const EdgeInsets.all(0),
+              children: List<Widget>.generate(daysInMonth, (i) {
+                return GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      selectedDate = DateTime(firstDayInMonth.year, firstDayInMonth.month, i+1);
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: _isDateSelected(i+1, firstDayInMonth.month, firstDayInMonth.year) ? theme.thirdColor : null,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                        child: Text(
+                          "${i+1}",
+                          style: TextStyle(
+                            color: _isDateSelected(i+1, firstDayInMonth.month, firstDayInMonth.year) ? Colors.white : theme.primaryColor,
+                          ),
+                        ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
