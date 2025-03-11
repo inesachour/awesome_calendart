bed488661ce0c62c4f834e94fa7469c469d8c80f
inesachour <ines.achour@insat.ucar.tn>
2025-03-11 10:32:35 +0100

    add documentation

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
+/// Class to define a customizable theme for a calendar (You can use predefined themes : [LightTheme] and [DarkTheme]).
 class AwesomeTheme {
+  /// The background color of the calendar.
   Color backgroundColor;
+
+  /// The color of the year and month text.
   Color yearAndMonthColor;
+
+  /// The color of the weekdays text.
   Color weekDaysColor;
+
+  /// The color of the next and previous buttons in the calendar.
   Color buttonColor;
+
+  /// The background color of a selected date.
   Color selectedDateBackgroundColor;
+
+  /// The color of the selected day text.
   Color selectedDayColor;
+
+  /// The color of the unselected days texts.
   Color unselectedDayColor;
 
   AwesomeTheme({
--- lib/src/widgets/awesome_calendart_widget.dart
+++ lib/src/widgets/awesome_calendart_widget.dart
@@ -3,14 +3,31 @@ import 'package:awesome_calendart/src/utils/date_utils.dart';
 import 'package:flutter/material.dart';
 
 class AwesomeCalenDart extends StatefulWidget {
+  /// The elevation of the calendar (default 5).
   final double elevation;
+
+  /// The border radius for rounded corners of the calendar (default 20)
   final double borderRadius;
+
+  /// The locale type for the calendar, determining the language (default [LocaleType.en]).
   final LocaleType locale;
+
+  /// A flag to indicate whether the full month name should be displayed (e.g., "January" instead of "Jan") (default false).
   final bool displayFullMonthName;
+
+  /// The text style for the year and month display. This value overrides the value passed in the theme's yearAndMonthColor property.
   final TextStyle? yearAndMonthTextStyle;
+
+  /// The text style for the weekdays display. This value overrides the value passed in the theme's weekDaysColor property.
   final TextStyle? weeksDaysTextStyle;
+
+  /// The text style for the selected days. This value overrides the value passed in the theme's selectedDayColor property.
   final TextStyle? selectedDaysTextStyle;
+
+  /// The text style for the unselected days. This value overrides the value passed in the theme's unselectedDayColor property.
   final TextStyle? unselectedDaysTextStyle;
+
+  /// A theme that can be applied to the calendar (default [LightTheme]).
   final AwesomeTheme? theme;
 
   const AwesomeCalenDart({
